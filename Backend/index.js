8973653421386c3da65ac8aa910be0a206859b4d
Rijo Sebastian <rijobdk@gmail.com>
2024-04-28 22:21:37 +0530

    Add signup route and database

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 dotenv.config();
 
@@ -26,6 +28,7 @@ try {
 
 // defining routes
 app.use("/book", bookRoute);
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
